31c8ccc05a4d8b1ea8e1964bc6a2792d985c6d52
Harry Turner <harry.turner@ultraleap.com>
2024-06-20 22:15:33 +0100

    Added login and logout functionality.

--- centour/article/views/article_index.py
+++ centour/article/views/article_index.py
@@ -1,5 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from article.models import Article
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = "article/index.html"
+    context_object_name = "articles"
+
+    def get_queryset(self):
+        return Article.objects.all()
--- centour/core/settings.py
+++ centour/core/settings.py
@@ -150,3 +150,5 @@ TAILWIND_APP_NAME = "theme"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+LOGIN_REDIRECT_URL = "index"
+LOGOUT_REDIRECT_URL = "index"
