aae58b2040798c998e56fad6acbb6799a907c3c8
harry-optimised <henry.j.turner@gmail.com>
2024-06-21 21:41:18 +0100

    Add user model to the admin.

--- centour/user/admin.py
+++ centour/user/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CentourUser
+
+
+@admin.register(CentourUser)
+class CentourUserAdmin(admin.ModelAdmin):
+    list_display = ("email", "username", "is_active", "is_staff", "is_superuser")
+    list_filter = ("is_active", "is_staff", "is_superuser")
+    search_fields = ("email", "username")
+    ordering = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "username")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "username", "password1", "password2"),
+        }),
+    )
